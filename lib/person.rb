ad79bcaaef7768f9a4c8d882955657a71c88a57b
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-10-13 21:00:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,21 @@ def initialize(name,job)
   @job = job
 end
 
+def name=(new_name)
+  @name = new_name
+end
+
+def name
+  @name
+end
+
+def job=(new_job)
+  @job = new_job
+end
+
+def job
+  @job
+end
 
   
   
